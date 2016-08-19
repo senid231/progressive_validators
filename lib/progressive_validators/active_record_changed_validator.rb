ce8744e1c6529b1eaa1950718e3feb282838bb52
Denis Talakevich <denis.t@didww.com>
2016-08-19 14:15:57 +0300

    fix namespace

--- lib/progressive_validators/active_record_changed_validator.rb
+++ lib/progressive_validators/active_record_changed_validator.rb
@@ -1,21 +1,23 @@
-class ActiveRecordChangedValidator < ActiveModel::Validator
-  # example:
-  # validates_with DecimalOverflowValidator
-  # validates_with DecimalOverflowValidator, message: 'exceeds limit'
-  # validates_with DecimalOverflowValidator, message: proc { |column, value| "#{value} is out of range" }
-  def validate(record)
-    return unless validator_allowable?
+module ProgressiveValidators
+  class ActiveRecordChangedValidator < ActiveModel::Validator
+    # example:
+    # validates_with DecimalOverflowValidator
+    # validates_with DecimalOverflowValidator, message: 'exceeds limit'
+    # validates_with DecimalOverflowValidator, message: proc { |column, value| "#{value} is out of range" }
+    def validate(record)
+      return unless validator_allowable?
 
-    record.changed.each do |changed_attribute|
-      validate_changed_attribute(record, changed_attribute)
+      record.changed.each do |changed_attribute|
+        validate_changed_attribute(record, changed_attribute)
+      end
     end
-  end
 
-  def validator_allowable?(record)
-    record.class.respond_to?(:columns_hash)
-  end
+    def validator_allowable?(record)
+      record.class.respond_to?(:columns_hash)
+    end
 
-  def validate_changed_attribute(_record, _attribute)
-  end
+    def validate_changed_attribute(_record, _attribute)
+    end
 
+  end
 end
